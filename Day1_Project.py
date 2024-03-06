3716f52797a4baa0e3b03d126e92f3111383d1cf
Chris <47773518+chirs-moon@users.noreply.github.com>
2024-03-06 20:38:23 +0100

    Added Day 1 Project - Simple Band Name Generator

---
+++ Day1_Project.py
@@ -0,0 +1,16 @@
+# Day 1 project from 100 Days of Code: The Complete Python Pro Bootcamp course
+
+# This is a program greeting
+print("Hi there! I will help you generate a name for your music band!")
+
+# Here the program creates variable "city" and asks user for input
+city = input("In what city did you grow up?\n")
+
+# Here the program creates variable "pet" and asks user for input
+pet = input("What is your pet's name?\n")
+
+# Here the program creates variable "band_name" and changes its value to the sum of two strings "city" and "pet"
+band_name = city + " " + pet
+
+# Here the program prints the variable "band_name"
+print("Your band name could be " + band_name)
