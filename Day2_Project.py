6bff736655523558caece68f6ea0c1821593f3f0
Chris <47773518+chirs-moon@users.noreply.github.com>
2024-03-07 16:50:37 +0100

    Added Day 2 Project - Simple Bill-splitting calculator

---
+++ Day2_Project.py
@@ -0,0 +1,18 @@
+# Day 2 project from 100 Days of Code: The Complete Python Pro Bootcamp course
+
+# This is a program greeting
+print("Welcome to the bill-splitting calculator.")
+
+# Here the program asks the user for three inputs and defines the data types of inputs
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15"))
+people = int(input("How many people to split the bill?"))
+
+# Here the program calculates the amount each person should pay
+total = bill + (bill * (tip / 100))
+per_person = round(total / people, 2)
+
+# Here we are changing the float to a string displaying two decimal places
+per_person = "{:.2f}".format(per_person)
+
+print(f"Each person should pay: ${per_person}")
