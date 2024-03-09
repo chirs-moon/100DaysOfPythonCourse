062febe445d417491ccb6c6ab7d252f478cdd2f8
Chris <47773518+chirs-moon@users.noreply.github.com>
2024-03-09 16:21:58 +0100

    Added Day 5 Project - Password Generator (Easy Version)

---
+++ Day5_Project_Easy.py
@@ -0,0 +1,30 @@
+# Day 5 project from 100 Days of Code: The Complete Python Pro Bootcamp course - Easy version
+import random
+
+print("Welcome to the PyPassword Generator!")
+letters_in = int(input("How many letter you like in your password?\n"))
+symbols_in = int(input("How many symbols would you like?\n"))
+numbers_in = int(input("How many numbers would you like?\n"))
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+           'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+password = ""
+
+for x in range(0, letters_in):
+    z = random.randint(0, len(letters) - 1)
+    password += letters[z]
+    print(z)
+
+for x in range(0, symbols_in):
+    z = random.randint(0, len(symbols) - 1)
+    password += symbols[z]
+
+for x in range(0, numbers_in):
+    z = random.randint(0, len(numbers) - 1)
+    password += numbers[z]
+
+print(f"Here is your password: {password}")
